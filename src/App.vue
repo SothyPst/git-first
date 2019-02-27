0ba9855a20ce418612449bffa1af8deebfddbf13
sothypen <sothy009.info@gmail.com>
2019-02-27 22:22:35 +0700

    fist commit to git

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+
+<template>
+  <div id="app" class="body">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <page1 msg="Page1 ......."> </page1>
+    <p>result: {{result}}</p>
+    <p>watch result: {{changeResult}}</p>
+    <button @click="sum">sum</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Page1 from "./Page1.vue";
+export default {
+  name: "app",
+  components: {
+    HelloWorld,
+    Page1
+  },
+  data() {
+    return {
+      value1: 10,
+      value2: 10,
+      result: 0,
+      changeResult: 'not changed'
+    };
+  },
+  methods: {
+    sum() {
+      this.result = this.value1 + this.value2;
+    }
+  },
+  watch: {
+    result(newValue){
+      this.changeResult = "changed"
+    }
+  }
+
+};
+</script>
+ 
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/Page1.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Hello World {{a+b}}</h1>
+    <h1>{{ msg }}</h1>
+    <button @click="sum" >Get Result: {{c}}</button> 
+    <h1 v-if="name">{{name}}</h1>
+    <input v-model="name"/>
+  </div>
+</template>
+<script>
+export default {
+  name: "Page1",
+  props: {
+    msg: String
+  },
+  data(){
+
+    return{
+      a: 2,
+      b: 3,
+      c:'',
+      name: ''
+    };
+  },
+  methods: {
+    sum() {
+      this.c = this.a + this.b;
+    }
+  },
+  watch: {
+    name(nextValue){
+console.log('name is changed')
+    }
+  }
+};
+</script>
+
+<style>
+</style>
+
